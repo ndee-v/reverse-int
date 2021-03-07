a7b9e5d5e690e46b5fcfccd91c62a4f37943d173
ndee-v <dronez86@gmail.com>
2021-03-07 21:51:26 +0600

    refactor: fill reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let num = n.toString().split('')
+  if (num[0] === "-") {
+      num.shift()
+  }
+  num = num.reverse()
+
+  while (num.lenth >= 1) {
+    if (num[0] === "0") {
+        num.shift()
+    } else {
+        break
+    }
+  }
+  return parseInt(num.join(''))
 }
